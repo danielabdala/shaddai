daa9b407fa91a7dfeb714cea81b237abc126f8c8
Daniel Abdala <danielabdalad@gmail.com>
2023-11-04 10:23:02 -0400

    Centralize headers and Actions. Add todo list for next coding session

--- src/components/Member.js
+++ src/components/Member.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { RiDeleteBin6Line } from 'react-icons/ri'
+import React from "react";
+import { AiOutlineEdit } from "react-icons/ai";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import "../css/Form.css";
 
-const Member = ({member,onEdit,onDelete}) =>
-{  
-     
-    const handledEditClick = () =>
-    {  
-        onEdit(member) 
-    }  
+const Member = ({ member, onEdit, onDelete }) => {
+  const handledEditClick = () => {
+    onEdit(member);
+  };
 
-    const handleDeleteClick = () =>
-    {
-         
-        onDelete(member)
-    }
-    
-    let content =   
-                <tr> 
-                    <td>{member.firstname}</td>
-                    <td>{member.lastname}</td>
-                    <td>{member.phone}</td>
-                    <td>{member.DOB}</td>
-                    <td><AiOutlineEdit title="Edit" onClick={handledEditClick}/>{' '}<RiDeleteBin6Line title="Delete" onClick={handleDeleteClick}/></td>
-                </tr> 
-    
-    return <>{content}</>;  
-}
+  const handleDeleteClick = () => {
+    onDelete(member);
+  };
+
+  let content = (
+    <tr>
+      <td>{member.firstname}</td>
+      <td>{member.lastname}</td>
+      <td>{member.phone}</td>
+      <td>{member.DOB}</td>
+      <td className="centered-cell">
+        <AiOutlineEdit title="Edit" onClick={handledEditClick} />{" "}
+        <RiDeleteBin6Line title="Delete" onClick={handleDeleteClick} />
+      </td>
+    </tr>
+  );
+
+  return <>{content}</>;
+};
 
 export default Member;
-
--- src/components/MemberListShow.js
+++ src/components/MemberListShow.js
@@ -1,150 +1,174 @@
-import React, {useEffect, useState} from 'react';
-import Member from './Member'
-import AddMember from './AddMember';
-import EditMember from './EditMember';
-import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container'; 
-import Form from 'react-bootstrap/Form';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button'; 
-import DeleteMemberConfirmation from './DeleteMemberConfirmation';
+import React, { useEffect, useState } from "react";
+import Member from "./Member";
+import AddMember from "./AddMember";
+import EditMember from "./EditMember";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import DeleteMemberConfirmation from "./DeleteMemberConfirmation";
+import "../css/Form.css";
 
+const MemberListShow = ({ members, onAdd, onEdit, onDelete }) => {
+  const [showAddForm, setShowAddForm] = useState(false);
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [rowToEdit, setRowToEdit] = useState({});
+  const [rowToDelete, setRowToDelete] = useState([]);
 
-const MemberListShow = ({members,onAdd, onEdit, onDelete}) => {  
+  //***ADD FUNCTIONS ***//
 
-  
-    const [showAddForm,setShowAddForm ] = useState(false)
-    const [showEditForm, setShowEditForm] = useState(false)
-    const [showDeleteModal, setShowDeleteModal] = useState(false)
-    const [rowToEdit, setRowToEdit] = useState({})
-    const [rowToDelete, setRowToDelete] = useState([])
+  const handleAddForm_ShowOrHide = () => {
+    setShowAddForm(!showAddForm);
+  };
 
-    //***ADD FUNCTIONS ***//
-  
-    const handleAddForm_ShowOrHide = () => {
-        setShowAddForm(!showAddForm); 
-    } 
+  const handleAddForm_OnSubmit = (newMember) => {
+    setShowAddForm(false);
+    onAdd(newMember);
+  };
 
-    const handleAddForm_OnSubmit = (newMember) => { 
-      
-        setShowAddForm(false);
-        onAdd(newMember); 
-    }  
+  //***DELETE FUNCTIONS ***//
 
-    //***DELETE FUNCTIONS ***//
-    
-    const handleShowDeleteForm = (member) =>
-    {   
-        setRowToDelete(member)
-        console.log("row to be deleted:",rowToDelete)
-        setShowDeleteModal(!showDeleteModal); 
-    }
+  const handleShowDeleteForm = (member) => {
+    setRowToDelete(member);
+    console.log("row to be deleted:", rowToDelete);
+    setShowDeleteModal(!showDeleteModal);
+  };
 
-    const handleDeleteModal_OnSubmit = () =>
-    {
-        onDelete(rowToDelete)
-        setShowDeleteModal(!showDeleteModal); 
-     
-    } 
-    const handleDeleteModal_OnClose = () =>
-    { 
-        setShowDeleteModal(!showDeleteModal);
-    }
+  const handleDeleteModal_OnSubmit = () => {
+    onDelete(rowToDelete);
+    setShowDeleteModal(!showDeleteModal);
+  };
+  const handleDeleteModal_OnClose = () => {
+    setShowDeleteModal(!showDeleteModal);
+  };
 
-    //*****EDIT FUNCTIONS *****/
+  //*****EDIT FUNCTIONS *****/
 
-    const handleShowEditForm = (member) => {
-        
-        setRowToEdit(member); // This sets the rowToEdit variable with the member value which is sent over as props to the EditMember Component 
-        setShowEditForm(!showEditForm); //check if state can just be false directly 
-    } 
+  const handleShowEditForm = (member) => {
+    setRowToEdit(member); // This sets the rowToEdit variable with the member value which is sent over as props to the EditMember Component
+    setShowEditForm(!showEditForm); //check if state can just be false directly
+  };
 
-    const handleEditForm_OnCancel = () => { 
-        setShowEditForm(!showEditForm); //check if state can just be false directly  
-    } 
+  const handleEditForm_OnCancel = () => {
+    setShowEditForm(!showEditForm); //check if state can just be false directly
+  };
 
-    const handleEditForm_OnSubmit = (editedMember) => { 
- 
-        setShowEditForm(false); 
-        onEdit(editedMember); 
-    }  
+  const handleEditForm_OnSubmit = (editedMember) => {
+    setShowEditForm(false);
+    onEdit(editedMember);
+  };
 
-    const renderedMembers = members.map((member) => 
-    {    
-      return ( 
-      <Member key={member.id} member={member} onEdit={handleShowEditForm} onDelete={handleShowDeleteForm}/> 
-       )  
-    } );      
-
-    let delete_modal = <DeleteMemberConfirmation show={showDeleteModal} onClose={handleDeleteModal_OnClose} onSubmit={handleDeleteModal_OnSubmit}></DeleteMemberConfirmation> 
- 
-    let main_content =   <div>  <Row>
-             
-            <Col></Col>
-         
-        <Col xs={8}>
-        <Form>
-            <Form.Group as={Row} className="mb-3">
-                <Col>
-                    <Button variant="primary" onClick={handleAddForm_ShowOrHide}>Add Member</Button>
-                </Col>
-            </Form.Group>
-        </Form>
-        </Col>
-       
-        <Col></Col>
-       
-        </Row>  
-        <Row>
-        <Col></Col>
-           <Col xs={8}>
-            <Table striped bordered hover>
-                <thead>
-                    <tr> 
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Phone Number</th>
-                        <th>Birth Day</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {renderedMembers}
-                </tbody>
-            </Table>
-           
-           </Col>
-          <Col></Col>
-            </Row>
-            </div>
-        
-
-    let content = showDeleteModal ? <div><Container>{main_content}{delete_modal}</Container></div> : <Container>{main_content}</Container> 
-    
-    // content = showEditForm ? <div><Container>{main_content}{edit_modal}</Container></div> : <Container>{main_content}</Container> 
- 
-    //if user clicks the add button -> show the add form
-    if(showAddForm) 
-        content =   <div>
-                        <AddMember show={showAddForm} onSubmit={handleAddForm_OnSubmit} onCancel={handleAddForm_ShowOrHide} members={members}></AddMember> 
-                    </div>  
-    //if users clicks the edit button -> show edit form 
-    if(showEditForm) 
-        content =   <div>
-                        {main_content} 
-                        <EditMember show={showEditForm} onSubmit={handleEditForm_OnSubmit} onCancel={handleEditForm_OnCancel} member={rowToEdit}></EditMember> 
-                    </div>  
-   
-        
+  const renderedMembers = members.map((member) => {
     return (
-        <div>  
-          <br/>
-          <br/> 
-                {content} 
-        </div> 
-    ) 
-    
-}
+      <Member
+        key={member.id}
+        member={member}
+        onEdit={handleShowEditForm}
+        onDelete={handleShowDeleteForm}
+      />
+    );
+  });
+
+  let delete_modal = (
+    <DeleteMemberConfirmation
+      show={showDeleteModal}
+      onClose={handleDeleteModal_OnClose}
+      onSubmit={handleDeleteModal_OnSubmit}
+    ></DeleteMemberConfirmation>
+  );
+
+  let main_content = (
+    <div>
+      {" "}
+      <Row>
+        <Col></Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={8}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th className="centered-cell">First Name</th>
+                <th className="centered-cell">Last Name</th>
+                <th className="centered-cell">Phone Number</th>
+                <th className="centered-cell">Birth Day</th>
+                <th className="centered-cell">Actions</th>
+              </tr>
+            </thead>
+            <tbody>{renderedMembers}</tbody>
+          </Table>
+        </Col>
+        <Col></Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col xs={2}>
+          <Form>
+            <Form.Group as={Row} className="mb-3">
+              <Col>
+                <Button variant="primary" onClick={handleAddForm_ShowOrHide}>
+                  Add Member
+                </Button>
+              </Col>
+            </Form.Group>
+          </Form>
+        </Col>
+        <Col></Col>
+      </Row>
+    </div>
+  );
+
+  let content = showDeleteModal ? (
+    <div>
+      <Container>
+        {main_content}
+        {delete_modal}
+      </Container>
+    </div>
+  ) : (
+    <Container>{main_content}</Container>
+  );
+
+  // content = showEditForm ? <div><Container>{main_content}{edit_modal}</Container></div> : <Container>{main_content}</Container>
+
+  //if user clicks the add button -> show the add form
+  if (showAddForm)
+    content = (
+      <div>
+        <AddMember
+          show={showAddForm}
+          onSubmit={handleAddForm_OnSubmit}
+          onCancel={handleAddForm_ShowOrHide}
+          members={members}
+        ></AddMember>
+      </div>
+    );
+  //if users clicks the edit button -> show edit form
+  if (showEditForm)
+    content = (
+      <div>
+        {main_content}
+        <EditMember
+          show={showEditForm}
+          onSubmit={handleEditForm_OnSubmit}
+          onCancel={handleEditForm_OnCancel}
+          member={rowToEdit}
+        ></EditMember>
+      </div>
+    );
+
+  return (
+    <div>
+      <br />
+      <br />
+      {content}
+    </div>
+  );
+};
 
 export default MemberListShow;
